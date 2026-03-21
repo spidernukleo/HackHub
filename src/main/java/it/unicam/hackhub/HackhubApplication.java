fc4f4335f5db7ceac0b3364285f6af2a204fff0d
spidernukleo <denisirmici04@gmail.com>
2026-03-21 01:46:01 +0100

    added code template

---
+++ src/main/java/it/unicam/hackhub/HackhubApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.hackhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackhubApplication.class, args);
+	}
+
+}
