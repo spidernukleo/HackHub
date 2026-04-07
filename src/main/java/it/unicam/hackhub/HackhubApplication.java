67e3453973f09f23e3f2b2b5f4046c48fb7d9e99
spidernukleo <denisirmici04@gmail.com>
2026-04-08 00:10:37 +0200

    Minor changes

--- src/main/java/it/unicam/hackhub/HackhubApplication.java
+++ src/main/java/it/unicam/hackhub/HackhubApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HackhubApplication {
 	public static void main(String[] args) {
-		System.out.println("Call schedulata con successo per l'hackathon "+1+"\nMentor: "+2+"\nTeam: "+3+"\nLink: "+ 4);
 		SpringApplication.run(HackhubApplication.class, args);
 	}
 
